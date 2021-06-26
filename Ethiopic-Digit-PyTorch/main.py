1d7e35126a70eb09220cd925ca99802ac6f0a78b
UmarZubair <umarzubair95@gmail.com>
2021-06-26 23:12:23 +0500

    Added files

--- Ethiopic-Digit-PyTorch/main.py
+++ Ethiopic-Digit-PyTorch/main.py
@@ -11,7 +11,7 @@ from torch.utils.data import random_split
 from utils import EthiopicDataset, make_csv, TestEthioipic
 
 TRAIN_DATA = 'data/train'
-TEST_DATA = 'data/test/test'
+TEST_DATA = 'data/test'
 MODEL_PATH = 'model/model.pth'
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
